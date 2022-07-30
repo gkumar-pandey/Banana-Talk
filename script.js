20c81d3cdd6d4242294a8eb2126ceef9d53f0f76
gkumar-pandey <gautamshekhar078@gmail.com>
2022-07-30 13:02:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const inputText = document.querySelector("#text-area");
+const translateBtn = document.querySelector(".trans-btn");
+const outputArea = document.querySelector(".output-area");
+
+const renderingTranslatedText = (text) => {
+  outputArea.append(text);
+};
+const fetchData = async (servelUrl) => {
+  try {
+    const res = await fetch(servelUrl);
+    const data = await res.json();
+    let trnaslatedText = data.contents.translated;
+    renderingTranslatedText(trnaslatedText);
+    return;
+  } catch (error) {
+    return null;
+  }
+};
+
+const translateHandle = () => {
+  const servelUrl = `https://api.funtranslations.com/translate/minion.json?text=${inputText.value}`;
+  fetchData(servelUrl);
+};
+
+translateBtn.addEventListener("click", translateHandle);
